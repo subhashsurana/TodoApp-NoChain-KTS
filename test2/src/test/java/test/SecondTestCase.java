a102d9a4b5388c59931332a309b10b258c5ad48e
mkjetbrains <maria.kudryavtseva@jetbrains.com>
2021-09-07 15:05:30 +0300

    Fail test

--- test2/src/test/java/test/SecondTestCase.java
+++ test2/src/test/java/test/SecondTestCase.java
@@ -1,6 +1,10 @@
 package test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SecondTestCase {
 
@@ -14,4 +18,19 @@ public class SecondTestCase {
 
     }
 
+    @Test
+    public void failed(){
+        Assertions.fail("must be failed this test");
+    }
+
+    @Test
+    @DisplayName("new test name")
+    public void someName(){
+    }
+
+    @ParameterizedTest(name = "my test({0})")
+    @ValueSource(strings = {"value1", "value2", "error"})
+    public void parameterized(String value) {
+        Assertions.assertNotEquals("error", value);
+    }
 }
